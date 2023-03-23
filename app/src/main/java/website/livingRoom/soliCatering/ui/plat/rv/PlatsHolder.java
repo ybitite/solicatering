44e0821aec26c9a63cd29be62c6189e662770561
ybitite <younes.bitite@gmail.com>
2023-03-23 03:20:17 +0100

    Plat Holder Refactoring

--- app/src/main/java/website/livingRoom/soliCatering/ui/plat/rv/PlatsHolder.java
+++ app/src/main/java/website/livingRoom/soliCatering/ui/plat/rv/PlatsHolder.java
@@ -20,88 +20,116 @@ import website.livingRoom.soliCatering.utile.Helper;
 
 public class PlatsHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
     //FIELD
-    website.livingRoom.soliCatering.ui.categorie.rv.ItemClickListener itemClickListener;
-    static ModelPlatsBinding binding;
-    static Context context;
+    private ItemClickListener itemClickListener;
+    private final ModelPlatsBinding binding;
+    private final Context context;
 
-    public PlatsHolder(View itemView) {
-        super(itemView);
+    public PlatsHolder(ModelPlatsBinding modelPlatsBinding) {
+        super(modelPlatsBinding.getRoot());
 
         //GET CONTEXT TO USE LATER
-        context = itemView.getContext();
-        //FIX WIDTH OF CARD LIKE WIDTH OF SCREEN
-        int width = context.getResources().getDisplayMetrics().widthPixels;
-        binding.cardViewCategorie.setMinimumWidth(width);
+        context = modelPlatsBinding.getRoot().getContext();
+
+        // instantiate binding object
+        binding = modelPlatsBinding;
+
         //SET CLICK LISTENER IN ITEM
         itemView.setOnClickListener(this);
     }
 
     //METHODE
+
     @Override
     public void onClick(View v) {
         this.itemClickListener.onItemClick(v, getLayoutPosition());
     }
-
-    public void setItemClickListener(website.livingRoom.soliCatering.ui.categorie.rv.ItemClickListener ic) {
+    public void setItemClickListener(ItemClickListener ic) {
         this.itemClickListener = ic;
     }
 
     public static PlatsHolder create(ViewGroup parent) {
         //GENERATE CLASS FROM MODEL EVENT
-        binding = ModelPlatsBinding
+        ModelPlatsBinding modelPlatsBinding = ModelPlatsBinding
                 .inflate(LayoutInflater.from(parent.getContext()));
 
-        return new PlatsHolder(binding.getRoot());
+        return new PlatsHolder(modelPlatsBinding);
     }
 
     public void bind(Plat plat, PlatViewModel platViewModel) {
 
+        fixWidth();
+
         //SET DATA IN VIEW
         binding.imageViewPlat.setImageResource(Helper.idResource(plat.getNomPic()));
-        binding.textViewTitrePlat.setText(plat.getNom());
+        binding.setPlat(plat);
 
-        //SET ICON VEGUI
-        if (plat.getVegui() == "o")
-            binding.imageViewVegi.setImageResource(R.drawable.icon_vegi);
-        else
-            binding.imageViewVegi.setImageResource(R.drawable.icon_vegi_no);
+        upDateIconVegi(plat);
+
+        updateIconEpice(plat);
+
+        updateItemClick(plat, platViewModel);
+    }
+
+    private void updateItemClick(Plat plat, PlatViewModel platViewModel) {
+        /*BLOCK CLICK AND MAKE ITEM GREW WHEN THE POINT ARE NOT ENOUGH*/
+        if (plat.getPoint() <= ConteurRepository.getCategorieActuel()) {
+
+            blockItem(View.INVISIBLE, true);
+
+            //navigate to dialog en click
+            setItemClickListener((v, pos) -> {
+                navigateToDialogPlat(plat, platViewModel); });
+        }
+        else {
+            blockItem(View.VISIBLE, false);
+        }
+    }
+
+    private void navigateToDialogPlat(Plat plat, PlatViewModel platViewModel) {
+        //SAVE SELECTED PLAT
+        platViewModel.setSelectedPlat(plat);
+        //NAVIGATE TO VIEW PLAT
+        Navigation.findNavController((Activity) context, R.id.nav_host_fragment_activity_main).navigate(R.id.action_navigation_plat_to_dialogPlat2);
+    }
+
+    private void blockItem(int visible, boolean clickable) {
+        //MAKE ITEM GRIS AND POINT GREEN
+        binding.griserItemPlat.setVisibility(visible);
+        binding.linearLayoutPlat.setClickable(clickable);
+    }
+
+    private void updateIconEpice(Plat plat) {
         //SET ICON EPICE
         if (plat.getDegureEpice() == 3)
-            binding.imageViewDegureEpice.setImageResource(R.drawable.icon_degure_epice_3);
+            setIconEpice(R.drawable.icon_degure_epice_3);
 
         else if (plat.getDegureEpice() == 2)
-            binding.imageViewDegureEpice.setImageResource(R.drawable.icon_degure_epice_2);
+            setIconEpice(R.drawable.icon_degure_epice_2);
+
         else
-            binding.imageViewDegureEpice.setImageResource(R.drawable.icon_degure_epice_1);
+            setIconEpice(R.drawable.icon_degure_epice_1);
+    }
 
-        /*BLOCK CLICK AND MAKE ITEM GREW WHEN THE POINT ARE NOT ENOUGH*/
-        //GET CURENT CATEGORIE POINT
-        int ptRest = ConteurRepository.getCategorieActuel();
-        //WHERE IT IS ENOUGH
-        if (plat.getPoint() <= ptRest) {
-            //MAKE ITEM VISIBLE AND POINT RED
-            binding.griserItemPlat.setVisibility(View.INVISIBLE);
-            binding.linearLayoutPlat.setClickable(true);
-            //OVERRIDE ON ITEM CLICK
-            setItemClickListener(new ItemClickListener() {
-                @Override
-                public void onItemClick(View v, int pos) {
-                    try {
-                        //SAVE SELECTED PLAT
-                        platViewModel.setSelectedPlat(plat);
-                        //NAVIGATE TO VIEW PLAT
-                        //TODO: beug / cause context / ok with try catch
-                        Navigation.findNavController((Activity) context, R.id.nav_host_fragment_activity_main).navigate(R.id.action_navigation_plat_to_dialogPlat2);
-                    } catch (Exception exception) {
-                    }
-                }
-            });
-        } else {
-            //MAKE ITEM GRIS AND POINT GREEN
-            binding.griserItemPlat.setVisibility(View.VISIBLE);
-            binding.linearLayoutPlat.setClickable(false);
+    private void upDateIconVegi(Plat plat) {
+        //SET ICON VEGUI
+        if (plat.getVegui() == "o")
+            setIconVegi(R.drawable.icon_vegi);
+        else
+            setIconVegi(R.drawable.icon_vegi_no);
+    }
 
-        }
+    private void setIconVegi(int drawble) {
+        binding.imageViewVegi.setImageResource(drawble);
+    }
+
+    private void setIconEpice(int drawble) {
+        binding.imageViewDegureEpice.setImageResource(drawble);
+    }
+
+    private void fixWidth() {
+        //FIX WIDTH OF CARD LIKE WIDTH OF SCREEN
+        int width = context.getResources().getDisplayMetrics().widthPixels;
+        binding.cardViewCategorie.setMinimumWidth(width);
     }
 
 }
